aa0a2c1d4915ec5052b45bd48e4a133e1137dec2
alfredoLABClogistica <146864054+alfredoLABClogistica@users.noreply.github.com>
2023-11-09 16:41:16 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ABC WMS',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
